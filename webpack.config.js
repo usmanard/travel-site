34a3282ed685f3d649f96409ad2eb849ceeeda01
Usman <us.arshad@gmail.com>
2020-05-19 22:09:41 +0500

    Updated the CSS and added some packages from npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const postCSSPlugins = [
+  require("postcss-import"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
   require("autoprefixer"),
@@ -18,7 +19,7 @@ module.exports = {
         test: /\.css$/i,
         use: [
           "style-loader",
-          "css-loader",
+          "css-loader?url=false",
           { loader: "postcss-loader", options: { plugins: postCSSPlugins } },
         ],
       },
